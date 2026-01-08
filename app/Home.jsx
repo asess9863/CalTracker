f8c43e21cd110c2d1727757cc4761c0bc500e994
asessions28 <asessions28@my.whitworth.edu>
2026-01-07 23:00:34 -0800

    added functionality. new homepage

---
+++ app/Home.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { Button, StyleSheet, Alert, TextInput, Text, View, Image } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { Link } from 'expo-router';
+
+const Home = () => {
+    return (
+        <SafeAreaProvider>
+            <SafeAreaView style={styles.container}>
+                <Text style={styles.title}>
+                    Home Page
+                </Text>
+                <Link href={"/"} style={styles.link}>back to login</Link>
+            </SafeAreaView>
+            <View style={styles.imageLocation}>
+                <Image
+                    style={styles.image}
+                    source={require('../assets/image.png')}
+                />
+            </View>
+        </SafeAreaProvider>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'top',
+        backgroundColor: 'black',
+        flex: 1,
+        padding: 24,
+    },
+    title: {
+        color: 'white',
+        textAlign: 'center',
+        fontSize: 40,
+        font: 'times new romans',
+    },
+    input: {
+        height: 40,
+        margin: 15,
+        borderWidth: 4,
+        borderColor: 'white',
+        padding: 10,
+    },
+    link: {
+        textAlign: 'center',
+        marginTop: 12,
+        color: 'white',
+    },
+    imageLocation:
+    {
+        alignItems: 'center',
+        justifyContent: 'bottom',
+    },
+    image:
+    {
+        width: 75,
+        height: 75,
+        justifyContent: 'flex-end',
+    },
+});
+
+
+export default Home;
---
+++ app/index.jsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import { Button, StyleSheet, Alert, TextInput, Text, View } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { Link } from 'expo-router';
+
+const Login = () => {
+
+  let username = 'Asessions28';
+  let password = 'P@$$w0rd';
+  let auth = false;
+  const [Usertext, onChangeUserText] = React.useState('');
+  const [Passtext, onChangePassText] = React.useState('');
+
+  function LoginAttempt() {
+    let Redirect = false;
+    if (username === Usertext && password === Passtext) {
+      console.log('Redirecting...');
+      Redirect = true;
+    }
+    else {
+      Alert.alert('incorrect username or password.');
+    }
+    return Redirect;
+  }
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <View>
+          <Text style={styles.title}>
+            The Better Macro Tracker
+          </Text>
+          <TextInput
+            style={styles.input}
+            placeholderTextColor={'#ffffffff'}
+            onChangeText={onChangeUserText}
+            placeholder='Enter Username'
+            value={Usertext}
+          />
+          <TextInput
+            style={styles.input}
+            placeholderTextColor={'#ffffffff'}
+            onChangeText={onChangePassText}
+            placeholder='Enter Password'
+            value={Passtext}
+          />
+          <Button
+            title='Log In'
+            onPress={() => auth = LoginAttempt()}
+          />
+          <Link href={"/Home"} style={styles.link}>Continue as a guest</Link>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    backgroundColor: '#000000ff',
+    flex: 1,
+    padding: 24,
+  },
+  title: {
+    color: '#ffffffff',
+    textAlign: 'center',
+    fontSize: 40,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 4,
+    borderColor: '#ffffffff',
+    color: 'white',
+    padding: 10,
+  },
+  link: {
+    textAlign: 'center',
+    marginTop: 12,
+    color: 'white',
+  },
+});
+
+
+export default Login;
